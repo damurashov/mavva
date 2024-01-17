59636f63fdf27e318c55ed85abf184b58d0fbc40
DmitryMurashov <dmtr.murashov@gmail.com>
2024-01-17 11:01:46 +0300

    [examples:read_all_mavlink_messages_from_serial] Enh | Logging

--- examples/read_all_mavlink_messages_from_serial.py
+++ examples/read_all_mavlink_messages_from_serial.py
@@ -4,7 +4,10 @@ import os
 
 
 def main():
-    connection = mavva.connectivity.make_serial_mavlink_connection(os.getenv("MAVVA_SERIAL", "/dev/ttyUSB0"), os.getenv("MAVVA_BAUDRATE", 115200))
+    serial = os.getenv("MAVVA_SERIAL", "/dev/ttyUSB0")
+    baudrate = os.getenv("MAVVA_BAUDRATE", 115200)
+    connection = mavva.connectivity.make_serial_mavlink_connection(serial, baudrate)
+    print(serial, baudrate)
     reader = mavva.connectivity.ThreadedMavlinkConnectionReader(connection)
     logger = mavva.snippet.LoggingMessageHandler()
     reader.add_message_handler(logger)
